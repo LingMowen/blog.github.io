4c4490c44a1e4a31fc94ae785e9efc97be4777b8
LingMowen <778741648@qq.com>
2025-07-12 11:37:27 +0800

    <p>最后更新时间：2025-07-12 11:37:23</p>

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/07/11/hello-world/"];function toRandomPost(){
+var posts=["2025/07/11/hello-world/","2025/07/12/迷你世界光影/","2025/07/12/测试/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
